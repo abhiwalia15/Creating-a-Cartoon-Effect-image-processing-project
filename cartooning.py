ec7e81a2beb24abd8abdc02cb6e27540a934c453
Mrinal <waliamrinal@gmail.com>
2019-03-18 14:35:34 +0530

    adding the effects

--- cartooning.py
+++ cartooning.py
@@ -4,14 +4,15 @@ import numpy as np
 #read theimage
 img = cv2.imread('Test Images/abhi.jpg')
 
+#threshold of gray scale image
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
 #checking out the edges
 edges = cv2.Canny(img, 75, 150)
 
-#checking the blurred image
-blur = cv2.medianBlur(img, 5)
-
-
-
+#checking the blurred image, the more we increase the blur ;lesser edges we will get
+blur = cv2.medianBlur(img, 7)
 
 
 
@@ -26,6 +27,10 @@ cv2.imshow("Image with Edges", edges)
 
 #show the blurred image
 cv2.imshow('Blurred image', blur)
+
+#show the gray image
+cv2.imshow('Gray image', gray)
+
 
 cv2.waitKey(0) 
 cv2.destroyAllWindows()
