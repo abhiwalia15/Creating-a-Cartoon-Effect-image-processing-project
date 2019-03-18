4a041c6faa89b009131ee5d6d64402152377fefa
Mrinal <waliamrinal@gmail.com>
2019-03-18 14:44:26 +0530

    chcnges made with effects git push

--- mainSRC.py
+++ mainSRC.py
@@ -1,11 +1,11 @@
-#import
+#import modules
 import cv2
 import numpy as np
 
-#read theimage
+#read the image
 img = cv2.imread('abhi.jpg')
 
-# 1) Edges
+# 1)first create the gray scale image with Edges
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.medianBlur(gray, 5)
 edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
