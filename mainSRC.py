1d9c21f59fd22930393e4a6a3901e427ddccf29b
Mrinal <waliamrinal@gmail.com>
2019-03-18 14:45:34 +0530

    colorise the img

--- mainSRC.py
+++ mainSRC.py
@@ -10,7 +10,8 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.medianBlur(gray, 5)
 edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
 
-# 2) Color
+
+# 2)Secondly we will Colorise the image using bilateral filter method
 color = cv2.bilateralFilter(img, 9, 300, 300)
 
 # 3) Cartoon
