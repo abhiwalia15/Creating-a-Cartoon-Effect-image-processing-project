48aa5c57e08ebf258dfd3fe2d40d9f74b0f0877e
Mrinal <waliamrinal@gmail.com>
2019-03-18 14:01:39 +0530

    folder with test images

--- cartooning.py
+++ cartooning.py
@@ -2,10 +2,18 @@ import cv2
 import numpy as np
 
 #read theimage
-img = cv2.imread('abhi.jpg')
+img = cv2.imread('Test Images/abhi.jpg')
 
-#check the image
-cv2.imshow('CARTOONED IMAGE", img)
-cv2.waitkey(0)
+#checking out the edges
+edges = cv2.Canny(img, 75, 150)
+
+#show the original image
+cv2.imshow("CARTOONED IMAGE", img)
+
+#show the images with edges
+cv2.imshow("Image with Edges", edges)
+
+
+cv2.waitKey(0)
 cv2.destroyAllWindows()
 
